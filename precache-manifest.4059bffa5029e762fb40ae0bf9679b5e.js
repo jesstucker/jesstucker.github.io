6a713df2babf049671c235c88e2591f3d52c8a27
jesstucker <jess.tucker@gmail.com>
2019-02-23 23:23:31 -0500

    update to react build

---
+++ precache-manifest.4059bffa5029e762fb40ae0bf9679b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "901c7c22f909ac5905a8",
+    "url": "/static/css/main.477bee25.chunk.css"
+  },
+  {
+    "revision": "901c7c22f909ac5905a8",
+    "url": "/static/js/main.901c7c22.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a76116d2102af47270fa",
+    "url": "/static/js/2.a76116d2.chunk.js"
+  },
+  {
+    "revision": "01643be9fa75a4523c77ac348b242aed",
+    "url": "/static/media/cmunrm.01643be9.ttf"
+  },
+  {
+    "revision": "aabada3bed00c55f2bfbc1473e323b2d",
+    "url": "/index.html"
+  }
+];
