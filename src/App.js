da632e559616a19337ed4a48eaec75cd94d6a038
jesstucker <jess.tucker@gmail.com>
2019-02-25 09:21:00 -0500

    refactor resume route to render on refresh, maybe, https bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import Resume from './Resume'
 import Paper from './Paper'
 
-import { Route, Link, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
@@ -13,7 +14,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Paper} />
-          <Route path="/resume" component={Resume}/>
+          <Route path="/resume" render={(routerProps) => <Resume router={routerProps}/>}/>
         </Switch>
         
       </div>
