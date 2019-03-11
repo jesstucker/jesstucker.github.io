431c7885319dc7850cc3066e83c8cb8ae400b124
jesstucker <jess.tucker@gmail.com>
2019-03-11 08:12:39 -0400

    Update meta title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `I am dumb`,
+    description: `blah blah blah`,
+    author: `me`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
