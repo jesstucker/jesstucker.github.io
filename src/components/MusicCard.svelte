97bc06b476127f8ef5b984b02ee897166400bdab
jesstucker <jess.tucker@gmail.com>
2021-12-10 00:19:48 -0500

    test to update

--- src/components/MusicCard.svelte
+++ src/components/MusicCard.svelte
@@ -16,28 +16,67 @@
 </script>
 
 <script>
+    import { each } from 'svelte/internal';
+    import { onMount } from 'svelte'
     import Notecard from './Notecard.svelte'
-    let tracks;
-   
-    function getTracks() {
-        // var fileReader = new FileReader();
+    let tracks = [],
+		current_song,
+		player,
+		play,
+		pause,
+		next,
+		playSelected;
 
-            fetch('./techno/musicList.json')
-                .then(resp => resp.json())
-                .then(foo => console.log(foo))
-                // .then(blob => console.log(typeof blob))
-                // .then(blob => fileReader.readAsArrayBuffer(blob))
-                // .then(blob => console.log(blob))
 
-        // new Audio('techno/;3-317235075.mp3').play()
-    }
+	onMount(async () => {
+		// let boo = new Audio('./techno/;3-317235075.mp3')
+		// boo.play()
+		// console.log(boo.src)
+		tracks = await fetch('./techno/musicList.json')
+					.then(resp => resp.json())
+					.then(data => tracks = data.map(t => t.filename))
+		current_song = 0;
+		player = new Audio();
+		player.src = tracks[current_song];
+
+		play = function (){
+			player.play();
+		}
+
+		pause = function (){
+			player.pause();
+		}
+
+		next = function (){
+			player.pause();
+			player.src = tracks[++current_song];
+			player.onload = () => player.play();
+		}
+		playSelected = function(song){
+			player.pause();
+			player.src = `./techno/${song}`;
+			console.log(player.src)
+			player.play();	
+		}
+	})
+     // Contains Audio URLs 
+
+
+    
     
     // const blah = preload()
 
 </script>
-<div >preload</div>
+
 <div>  </div>
 <div></div>
-<div 
-  on:click={getTracks} 
-  >clickme</div>
+<!-- <div on:click={next}>clickme</div> -->
+
+<!-- <button on:click={play}>play</button> -->
+<ul>
+	{#each tracks as track}
+		<li on:click={() => playSelected(track)}>
+			{track}
+    	</li>
+	{/each}
+</ul>
