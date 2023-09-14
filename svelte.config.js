2da60ca59a5e378e5c51878ed41cfbe234780258
Jess-Tucker_brp <jess.tucker@baldwinriskparters.com>
2023-09-13 19:15:57 -0500

    use npm ci

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.argv.includes('dev');
+console.log('dev:', dev)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,7 @@ const config = {
 
 		}),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+            base: process.env.BASE_PATH,
         }
     }
 };
