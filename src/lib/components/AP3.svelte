3b44acd7df19ce174bb13450d1257fa354d882fe
Jess-Tucker_brp <jess.tucker@baldwinriskparters.com>
2023-09-16 16:43:47 -0500

    wip

--- src/lib/components/AP3.svelte
+++ src/lib/components/AP3.svelte
@@ -2,146 +2,143 @@
 	import { onMount } from "svelte";
     let audio;
     $: console.log(audio)
+    let playlist = getAwesomePlaylist();
+        let index = 0;
 
     function onPlayButtonClick() {
             playAudio();
         }
-
+    function playAudio() {
+        audio.src = playlist[index].src;
+        audio.play()
+        .then(_ => updateMetadata())
+        .catch(error => console.log(error));
+    }
 
         // let audio = document.createElement('audio');
 
-        let playlist = getAwesomePlaylist();
-        let index = 0;
+        onMount(() => {
+            // audio = document.querySelector('audio');
+            // console.log(audio)
 
 
-
-        function playAudio() {
-            audio.src = playlist[index].src;
-            audio.play()
-            .then(_ => updateMetadata())
-            .catch(error => console.log(error));
-        }
 
         function updateMetadata() {
             let track = playlist[index];
 
             console.log('Playing ' + track.title + ' track...');
+
+            navigator.mediaSession.metadata = new MediaMetadata({
+                title: track.title,
+                artist: track.artist,
+                album: track.album,
+                artwork: track.artwork
+            });
         }
 
-        onMount(() => {
-        //     audio = document.querySelector('audio');
-        //     console.log(audio)
-        
-        // navigator.mediaSession.metadata = new MediaMetadata({
-        //     title: track.title,
-        //     artist: track.artist,
-        //     album: track.album,
-        //     artwork: track.artwork
-        // });
-
-        // // Media is loaded, set the duration.
-        // updatePositionState();
+        // Media is loaded, set the duration.
+        updatePositionState();
 
 
-        // /* Position state (supported since Chrome 81) */
+        /* Position state (supported since Chrome 81) */
 
-        // function updatePositionState() {
-        // if ('setPositionState' in navigator.mediaSession) {
-        //     console.log('Updating position state...');
-        //     navigator.mediaSession.setPositionState({
-        //     duration: audio.duration,
-        //     playbackRate: audio.playbackRate,
-        //     position: audio.currentTime
-        //     });
-        // }
-        // }
+        function updatePositionState() {
+        if ('setPositionState' in navigator.mediaSession) {
+            console.log('Updating position state...');
+            navigator.mediaSession.setPositionState({
+            duration: audio.duration,
+            playbackRate: audio.playbackRate,
+            position: audio.currentTime
+            });
+        }
+        }
 
-        // /* Previous Track & Next Track */
+        /* Previous Track & Next Track */
 
-        // navigator.mediaSession.setActionHandler('previoustrack', function() {
-        // console.log('> User clicked "Previous Track" icon.');
-        // index = (index - 1 + playlist.length) % playlist.length;
-        // playAudio();
-        // });
+        navigator.mediaSession.setActionHandler('previoustrack', function() {
+        console.log('> User clicked "Previous Track" icon.');
+        index = (index - 1 + playlist.length) % playlist.length;
+        playAudio();
+        });
 
-        // navigator.mediaSession.setActionHandler('nexttrack', function() {
-        // console.log('> User clicked "Next Track" icon.');
-        // index = (index + 1) % playlist.length;
-        // playAudio();
-        // });
+        navigator.mediaSession.setActionHandler('nexttrack', function() {
+        console.log('> User clicked "Next Track" icon.');
+        index = (index + 1) % playlist.length;
+        playAudio();
+        });
 
-        // audio.addEventListener('ended', function() {
-        // // Play automatically the next track when audio ends.
-        // index = (index - 1 + playlist.length) % playlist.length;
-        // playAudio();
-        // });
+        audio.addEventListener('ended', function() {
+        // Play automatically the next track when audio ends.
+        index = (index - 1 + playlist.length) % playlist.length;
+        playAudio();
+        });
 
-        // /* Seek Backward & Seek Forward */
+        /* Seek Backward & Seek Forward */
 
-        // let defaultSkipTime = 10; /* Time to skip in seconds by default */
+        let defaultSkipTime = 10; /* Time to skip in seconds by default */
 
-        // navigator.mediaSession.setActionHandler('seekbackward', function(event) {
-        // console.log('> User clicked "Seek Backward" icon.');
-        // const skipTime = event.seekOffset || defaultSkipTime;
-        // audio.currentTime = Math.max(audio.currentTime - skipTime, 0);
-        // updatePositionState();
-        // });
+        navigator.mediaSession.setActionHandler('seekbackward', function(event) {
+        console.log('> User clicked "Seek Backward" icon.');
+        const skipTime = event.seekOffset || defaultSkipTime;
+        audio.currentTime = Math.max(audio.currentTime - skipTime, 0);
+        updatePositionState();
+        });
 
-        // navigator.mediaSession.setActionHandler('seekforward', function(event) {
-        // console.log('> User clicked "Seek Forward" icon.');
-        // const skipTime = event.seekOffset || defaultSkipTime;
-        // audio.currentTime = Math.min(audio.currentTime + skipTime, audio.duration);
-        // updatePositionState();
-        // });
+        navigator.mediaSession.setActionHandler('seekforward', function(event) {
+        console.log('> User clicked "Seek Forward" icon.');
+        const skipTime = event.seekOffset || defaultSkipTime;
+        audio.currentTime = Math.min(audio.currentTime + skipTime, audio.duration);
+        updatePositionState();
+        });
 
-        // /* Play & Pause */
+        /* Play & Pause */
 
-        // navigator.mediaSession.setActionHandler('play', async function() {
-        // console.log('> User clicked "Play" icon.');
-        // await audio.play();
-        // // Do something more than just playing audio...
-        // });
+        navigator.mediaSession.setActionHandler('play', async function() {
+        console.log('> User clicked "Play" icon.');
+        await audio.play();
+        // Do something more than just playing audio...
+        });
 
-        // navigator.mediaSession.setActionHandler('pause', function() {
-        // console.log('> User clicked "Pause" icon.');
-        // audio.pause();
-        // // Do something more than just pausing audio...
-        // });
+        navigator.mediaSession.setActionHandler('pause', function() {
+        console.log('> User clicked "Pause" icon.');
+        audio.pause();
+        // Do something more than just pausing audio...
+        });
 
-        // audio.addEventListener('play', function() {
-        // navigator.mediaSession.playbackState = 'playing';
-        // });
+        audio.addEventListener('play', function() {
+        navigator.mediaSession.playbackState = 'playing';
+        });
 
-        // audio.addEventListener('pause', function() {
-        // navigator.mediaSession.playbackState = 'paused';
-        // });
+        audio.addEventListener('pause', function() {
+        navigator.mediaSession.playbackState = 'paused';
+        });
 
-        // /* Stop (supported since Chrome 77) */
+        /* Stop (supported since Chrome 77) */
 
-        // try {
-        // navigator.mediaSession.setActionHandler('stop', function() {
-        //     console.log('> User clicked "Stop" icon.');
-        //     // TODO: Clear UI playback...
-        // });
-        // } catch(error) {
-        // console.log('Warning! The "stop" media session action is not supported.');
-        // }
+        try {
+        navigator.mediaSession.setActionHandler('stop', function() {
+            console.log('> User clicked "Stop" icon.');
+            // TODO: Clear UI playback...
+        });
+        } catch(error) {
+        console.log('Warning! The "stop" media session action is not supported.');
+        }
 
-        // /* Seek To (supported since Chrome 78) */
+        /* Seek To (supported since Chrome 78) */
 
-        // try {
-        // navigator.mediaSession.setActionHandler('seekto', function(event) {
-        //     console.log('> User clicked "Seek To" icon.');
-        //     if (event.fastSeek && ('fastSeek' in audio)) {
-        //     audio.fastSeek(event.seekTime);
-        //     return;
-        //     }
-        //     audio.currentTime = event.seekTime;
-        //     updatePositionState();
-        // });
-        // } catch(error) {
-        // console.log('Warning! The "seekto" media session action is not supported.');
-        // }
+        try {
+        navigator.mediaSession.setActionHandler('seekto', function(event) {
+            console.log('> User clicked "Seek To" icon.');
+            if (event.fastSeek && ('fastSeek' in audio)) {
+            audio.fastSeek(event.seekTime);
+            return;
+            }
+            audio.currentTime = event.seekTime;
+            updatePositionState();
+        });
+        } catch(error) {
+        console.log('Warning! The "seekto" media session action is not supported.');
+        }
 
         /* Utils */
     })
