29c5f5de95af98cdd3e4c9c6e0fefd73d097840c
Jess-Tucker_brp <jess.tucker@baldwinriskparters.com>
2023-09-20 19:51:08 -0500

    wip

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,11 @@ export function formatTimer(sec: number) {
     const seconds = Math.floor(sec - min * 60);
     return `${String(min).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
 }
+
+export function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
